ad099a773fcbe15d41e92534fb4e7ae343b231ad
nicolastradivarius <gonzalez.125251@alu.uns.edu.ar>
2026-01-10 17:45:42 -0300

    Refactor import path for PrismaClient to use alias for improved readability

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from '../app/generated/prisma';
+import { PrismaClient } from '@/app/generated/prisma';
 import { PrismaPg } from '@prisma/adapter-pg'
 import 'dotenv/config'
 import bcrypt from 'bcryptjs';
